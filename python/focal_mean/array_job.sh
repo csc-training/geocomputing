8a936661a4f12d050b0955b129673a95d982a6bc
Kylli Ek <kylli.ek@csc.fi>
2017-08-22 10:13:45 +0300

    added loading module, fixed SLURM partition

--- python/focal_mean/array_job.sh
+++ python/focal_mean/array_job.sh
@@ -6,9 +6,14 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2
 #SBATCH --array=1-2
-#SBATCH -p test
+#SBATCH -p serial
+
+# load needed modules
+module load geopython
+# move to the directory where the data files locate
+cd ~/input_files_folder
 
 # read file names to be processed from a list given as an argument
-name=$(sed -n "$SLURM_ARRAY_TASK_ID"p $1)
+name=$(sed -n "$SLURM_ARRAY_TASK_ID"p mapsheets.txt)
 # run the analysis command and save results to smooth/ directory
 python simple_focal_mean.py $name
