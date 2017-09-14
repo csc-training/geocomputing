58afeec1c42fa63253c518bb81dc917e46a20418
Kylli Ek <kylli.ek@csc.fi>
2017-09-14 14:27:28 +0300

    Create simple_batch_job.sh

---
+++ R/contours/simple/simple_batch_job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J rspatial_job
+#SBATCH -o output.txt
+#SBATCH -e errors.txt
+#SBATCH -t 0:00:05
+#SBATCH -p test
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=1000
+#
+module load rspatial-env
+srun Rscript --no-save Contours_simple.R
