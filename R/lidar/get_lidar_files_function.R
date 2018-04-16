c63fea7270ac35915f53182e0f9739f4f3f79ae7
egonzale <eglatorre+t1@gmail.com>
2018-04-16 07:55:29 +0300

    get lidar files intersecting a polygon, function

---
+++ R/lidar/get_lidar_files_function.R
@@ -0,0 +1,36 @@
+#####################
+#### Get Lidar file names as a function
+#####################
+lidar_files_taito <- function(f_poly){
+  # Get lidar file names intersecting with given polygon
+  # Note that in this example only year 2013 lidar index is used
+  
+  library(raster)
+  
+  # The lidar_index to be used
+  f_lidar_index <- "/wrk/project_ogiir-csc/mml/laser/Automaattisen luokittelu 2008 - 2016/Automaattinen_luokittelu_indeksi/2013/Export_AUTO_PISTEPILVITIEDOSTO_2013.shp"
+  lidar_index       <<- shapefile(f_lidar_index)
+  
+  # Polygon to get lidar datasets from
+  f_poly <- "area_of_interest.shp"
+  poly <- shapefile(f_poly)
+  
+  # What lidar index tiles intersect with our polygon
+  inters <- intersect(lidar_index, poly)
+  if (is.null(inters)) {
+    print("ERROR: LiDAR tiles do not cover the study area!!!!")
+    next
+  }
+  
+  # Query file names are intersecting
+  files <- vector()
+  for (i in 1:length(inters@polygons)){
+    file <- paste0(inters$TUNNUS[i],".laz")
+    files <- c(files, file)
+  }
+  
+  # Transform to current Taito path
+  files_taito <- gsub("laser/automaattinen/",
+                      "/wrk/project_ogiir-csc/mml/laser/Automaattisen luokittelu 2008 - 2016/", files)
+  return(files_taito)
+}
