91abc28069a40756619dbcc3cac13519e719b182
Elias Annila <eannila@h820g3l-153k63.x.csc.fi>
2018-11-16 14:43:41 +0200

    added foreach-dompi example for R contours

---
+++ R/contours/parallel_foreach/Calc_contours_foreach.R
@@ -0,0 +1,66 @@
+# This is an spatial analysis example script for using R in CSC Taito
+# This script can be used for parallel jobs.
+# Here 3 .tif files are saved to SagaGIS format
+# and then countours are calculated and saved in Shape format.
+# The file given as input is a 10m DEM file from Finnish NLS.
+# The input files are listed in the mapsheet.txt file
+
+# For parallel tasks the foreach with doMPI is used.
+# See https://research.csc.fi/-/r
+
+library(doMPI,quietly=TRUE)
+cl<-startMPIcluster()
+registerDoMPI(cl)
+
+#Set directory for output files (in this case from user's workdirectory): 
+mainDir <- file.path(Sys.getenv("WRKDIR"), "R_contours_foreach")
+gridFolder <- file.path(mainDir, "1_grid")
+shapeFolder <- file.path(mainDir, "2_shape")
+
+# Read the mapsheets from external file, in this case from user's workdirectory
+mapsheets <- readLines(file.path(Sys.getenv("WRKDIR"),'geocomputing/R/contours/mapsheets.txt'))
+
+
+# Set the working directory
+if (!dir.exists(mainDir)) {
+  dir.create(mainDir)
+}
+
+#Make folders for files, do not make, if already exist
+if (!dir.exists(shapeFolder)) {
+  dir.create(shapeFolder)
+}
+
+if (!dir.exists(gridFolder)) {
+  dir.create(gridFolder)
+}
+
+# The function run on each core
+# The R modules need to be loaded inside the functions.
+# The variables from outside of this function are not visible.
+
+funtorun<-function(mapsheet) {
+  library(RSAGA)
+  library(gdalUtils)
+
+  # SagaGIS needs the input in its own format, so let's change the original .tif file to Saga format.
+  gridfile <- file.path(gridFolder, gsub("tif", "sdat", basename(mapsheet)))
+  gdal_translate(mapsheet, gridfile, of="SAGA",verbose=TRUE)
+
+
+  # Calculate contours with 50m intervals, from 200 to 750m
+  shapefile <- file.path(shapeFolder, gsub("tif", "shp", basename(mapsheet)))
+  gridfile <- gsub("sdat", "sgrd", gridfile)
+  rsaga.contour(gridfile, shapefile, 50, 200, 750, env = rsaga.env())
+
+  #The tool gives like other RSAGA tools "Error: select a tool" message, but it actually is working :)
+}
+
+# Run funtorun function in parallel for each mapsheet
+a<-foreach(i=1:3) %dopar% {
+	funtorun(mapsheets[i])
+}
+
+closeCluster(cl)
+mpi.quit()
+
---
+++ R/contours/parallel_foreach/parallel_batch_job_foreach.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH -J r_multi_proc
+#SBATCH -o output.txt
+#SBATCH -e errors.txt
+#SBATCH -t 00:01:00
+#SBATCH --ntasks=3
+#SBATCH -p parallel
+#SBATCH --mem-per-cpu=1000
+
+module load rspatial-env
+srun Rscript --no-save --slave Calc_contours_foreach.R
