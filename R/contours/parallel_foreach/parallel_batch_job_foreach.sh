e18fd03fee6ae0c7265aade83791758568a77a39
Elias Annila <eannila@h820g3l-153k63.x.csc.fi>
2018-11-20 14:17:11 +0200

    added .export and .packages to R contours parallel foreach example

--- R/contours/parallel_foreach/parallel_batch_job_foreach.sh
+++ R/contours/parallel_foreach/parallel_batch_job_foreach.sh
@@ -4,6 +4,7 @@
 #SBATCH -e errors.txt
 #SBATCH -t 00:01:00
 #SBATCH --ntasks=3
+#Test partition is used for testing, for real jobs use either serial or parallel depending on how many nodes you need.
 #SBATCH -p test
 #SBATCH --mem-per-cpu=1000
 
