1a39bf46c490aa65e5e55f8aa3a9eaddc9666c97
johannesnyman <54168685+johannesnyman@users.noreply.github.com>
2019-11-04 18:42:25 +0200

    Update 05_classification.sh

--- machineLearning/shallows/05_classification.sh
+++ machineLearning/shallows/05_classification.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=classificationExcercise
 #SBATCH --account=project_2002044
-#SBATCH --time=00:15:00
+#SBATCH --time=00:30:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=2G
 #SBATCH --partition=small
 
