2add53a42e4d597c4cc4fa8570d70fc596a9acff
Kylli Ek <kylli.ek@csc.fi>
2019-12-04 13:38:22 +0200

    Add Allas_R

---
+++ working_with_allas_from_R.R
@@ -0,0 +1,53 @@
+# Example script for using Allas directly from an R script:
+# - Reading raster and vector files
+# - Looping over all files of certain type in a bucket
+# - Writing raster and vector files (not working properly)
+
+library("raster")
+library("sf")
+library("aws.s3")
+library("tidyverse")
+
+# Before starting to use Allas with aws.s3 set up your connection to Allas, as described here: https://docs.csc.fi/#apps/gdal/
+# (Run allas-conf --mode s3cmd and create .aws/credentials file.) 
+# Setting the S3 endpoint can be done with R:
+Sys.setenv("AWS_S3_ENDPOINT" = "a3s.fi")
+
+# Reading raster file
+r <- raster('/vsis3/name_of_your_Allas_bucket/name_of_your_input_raster_file.tif')
+
+# Reading vector file
+v <- st_read('/vsis3/name_of_your_Allas_bucket/name_of_your_input_vector_file.gpkg')
+
+# Looping through all files in a bucket, having the same file type (tif).
+# First get list of all objects in the bucket
+all_files_in_bucket <- get_bucket_df(name_of_your_Allas_bucket)
+# Filter out only .tif-files and keep only the file name information (=Key)
+tif_files = all_files_in_bucket %>% filter(str_detect(Key, '.tif$')) %>% select(Key)
+# Loop through the files, here just printing the extent of each file as example.
+for (row in 1:nrow(tif_files)) {    
+  filepath <- paste('/vsis3/name_of_your_Allas_bucket/', tif_files[row,], sep = "")
+  print (filepath)
+  r <- raster(filepath)
+  print (extent(r))
+}
+
+# Writing files.
+# Not recommended to use like this.. 
+# For some reason R can not read these files back again, 
+# although the file is created and GDAL-commandline tools can read them and also if downloaded and opened with QGIS, 
+# then everything seems to be ok.. 
+
+# Writing raster file
+s3write_using(r, FUN = raster::writeRaster,
+              bucket = "name_of_your_Allas_bucket",
+              object = "name_of_your_output_raster_file.tif")
+
+
+# Writing vector file
+s3write_using(v, FUN = sf::st_write, layer='name_of_your_output_layer',
+              bucket = "name_of_your_Allas_bucket",
+              object = "name_of_your_output_vector_file.gpkg")
+
+
+
