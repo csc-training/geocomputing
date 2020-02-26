1c91a56ab0777dbff9caffed0e63601a3ff2f92f
johannesnyman <54168685+johannesnyman@users.noreply.github.com>
2020-02-26 13:27:41 +0200

    updated the excercise

--- R/contours/02_array/Contours_array.R
+++ R/contours/02_array/Contours_array.R
@@ -1,30 +1,28 @@
 # This is an spatial analysis example script for using R in CSC Puhti
-# This scipt can be used for serial or array jobs.
-# Here a .tif file provided as argument is save to SagaGIS format
+# This script can be used for serial or array jobs.
+# Here a .tif DEM file is provided as an argument
 # and then countours are calculated and saved in Shape format.
 # The file given as input is a 10m DEM file from Finnish NLS.
 
-#Read the command line argument, which is the path of the .tif file.
+# Load the necessary libraries
+library(raster)
+
+# Read the command line argument, which is the path of the .tif file.
 args = commandArgs(trailingOnly=TRUE)
 
 if (length(args)==0) {
   stop("Please give the map sheet number", call.=FALSE)
 } else if (length(args)==1) {
-  # default output file
+  # The filepath given to this script goes to variable mapsheet
   mapsheet <- args[1]
 }
 print(mapsheet)
-# load RSAGA and gdalUtils libraries
-library(raster)
 
-#Set the names of the folders.
-mainDir <- "/scratch/project_2002044/students/training011/R_spatial_exercises/02_array"
+# Set the working directory to the current directory
+setwd(getSrcDirectory()[1])
+
+# Create output folder if it does not exist
 shapeFolder <- "shape"
-
-# Set the working directory
-setwd(mainDir)
-
-#Make folders for files, do not make, if already exist
 if (!dir.exists(shapeFolder)) {
   dir.create(shapeFolder)
 }
@@ -34,5 +32,7 @@ DEM <- raster(mapsheet)
 shapefile <- file.path(shapeFolder, gsub("tif", "shp", basename(mapsheet)))
 contours<-rasterToContour(DEM)
 shapefile(contours, filename=shapefile, overwrite=TRUE)
+
+# If working locally, you can plot both files
 plot(DEM)
 plot(contours, add=TRUE)
