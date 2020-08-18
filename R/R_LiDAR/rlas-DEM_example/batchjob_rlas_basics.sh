e47fd120a15068afc9b51a8b7705b9015361e1e8
johannesnyman <54168685+johannesnyman@users.noreply.github.com>
2020-08-18 14:31:46 +0300

    Update batchjob_rlas_basics.sh

--- R/R_LiDAR/rlas-DEM_example/batchjob_rlas_basics.sh
+++ R/R_LiDAR/rlas-DEM_example/batchjob_rlas_basics.sh
@@ -9,4 +9,11 @@
 #SBATCH -n 1
 
 module load r-env-singularity
+
+# If you have installed packages this helps resolve problems related to those
+if test -f ~/.Renviron; then
+    sed -i '/TMPDIR/d' ~/.Renviron
+fi
+
+
 srun singularity_wrapper exec Rscript --no-save --slave basic_rlas.R
