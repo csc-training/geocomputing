e2b6a682fa5812b8954f0dad897a170552081ae6
Kylli Ek <kylli.ek@csc.fi>
2020-11-12 12:29:15 +0200

    singularity_wrapper

--- machineLearning/03_deep/06_deepRegression.sh
+++ machineLearning/03_deep/06_deepRegression.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=deepRegression
-#SBATCH --account=project_2002044
+#SBATCH --account=<your_project>
 #SBATCH --mem=16G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --time=00:10:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
-#SBATCH --reservation=ml_training_10
 
 module load tensorflow/nvidia-19.11-tf2-py3
-
-srun python 06_deepRegression.py
+srun singularity_wrapper exec python 06_deepRegression.py
