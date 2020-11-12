1012430349270d18a87adb3b9b3be24103fb4a04
Kylli Ek <kylli.ek@csc.fi>
2020-11-12 12:31:51 +0200

    remove reservation

--- machineLearning/05_cnn_keras/09_2_predict.sh
+++ machineLearning/05_cnn_keras/09_2_predict.sh
@@ -6,7 +6,6 @@
 #SBATCH --mem=4G
 #SBATCH --time=0:14:00
 #SBATCH --gres=gpu:v100:1
-#SBATCH --reservation ml10
 
 module load tensorflow/nvidia-19.11-tf2-py3
 srun singularity_wrapper exec python3 09_2_predict.py
