dc1eeb5cc20ed4474feaafccb9b8c355c0082dba
Samantha Wittke <32324155+samumantha@users.noreply.github.com>
2021-11-04 11:25:23 +0200

    update to longe batch job options

--- R/puhti/05_parallel_future/parallel_batch_job_future_cluster.sh
+++ R/puhti/05_parallel_future/parallel_batch_job_future_cluster.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH --account=project_2002044
-#SBATCH -J r_multi_proc
-#SBATCH -o output.txt
-#SBATCH -e errors.txt
-#SBATCH -t 00:05:00
+#SBATCH --job-name=r_multi_proc
+#SBATCH --output=output.txt
+#SBATCH --error=errors.txt
+#SBATCH --time=00:05:00
 #Reserve cores for 1 master + 3 workers
 #SBATCH --ntasks=4
 #Test partition is used for testing, for real jobs use either serial or parallel depending on how many nodes you need.
-#SBATCH -p test
+#SBATCH -partition=test
 #SBATCH --mem-per-cpu=1000
 
 module load r-env-singularity
