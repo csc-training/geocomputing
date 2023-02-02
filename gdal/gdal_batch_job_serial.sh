f92afbcfc90819b4b4c802146f6632a691ea25f5
Kylli Ek <kylli.ek@csc.fi>
2023-02-02 16:12:14 +0200

    Add comments

--- gdal/gdal_batch_job_serial.sh
+++ gdal/gdal_batch_job_serial.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #SBATCH --account=project_2004306
-#SBATCH -J gdal_job
-#SBATCH -o out.txt
-#SBATCH -e err.txt
-#SBATCH -t 0:05:00
-#SBATCH -p test
+#SBATCH --job-name gdal_job
+#SBATCH --output out.txt
+#SBATCH --error err.txt
+#SBATCH --time 0:05:00
+#SBATCH --partition test
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=300
 
+# Load geoconda module to have GDAL commandline tools available.
 module load geoconda
 
-srun bash gdal_serial.sh
+# Run the bash script, which includes the GDAL commands.
+bash gdal_serial.sh
