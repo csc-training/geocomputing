b60606369273ff0978cea827df504f3a6b455c0b
Kylli Ek <kylli.ek@csc.fi>
2023-02-02 16:09:28 +0200

    Write SBATCH elements as long

--- gdal/gdal_batch_job_parallel.sh
+++ gdal/gdal_batch_job_parallel.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 #SBATCH --account=project_2004306
-#SBATCH -J gdal_job
-#SBATCH -o out.txt
-#SBATCH -e err.txt
-#SBATCH -t 0:05:00
-#SBATCH -p test
+#SBATCH --job-name gdal_job
+#SBATCH --output out.txt
+#SBATCH --error err.txt
+#SBATCH --time 0:05:00
+#SBATCH --partition test
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=300
 
 module load parallel geoconda
 
-#Find the files that have .tif ending, we do not want to process the .tif.aux.xml files in the same folders.
+# Find the files that have .tif ending, we do not want to process the .tif.aux.xml files in the same folders.
+# Run the GDAL script for each found file.
 
 find find /appl/data/geo/mml/dem10m/2019/W3/W33 -name '*.tif' | \
     parallel -j $SLURM_CPUS_PER_TASK bash gdal_parallel.sh {}
