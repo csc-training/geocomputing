ae03e0065884aec35dfe1bb84d65cc7a7959fc14
Kylli Ek <kylli.ek@csc.fi>
2023-10-04 17:14:35 +0300

    -overwrite

--- gdal/gdal_parallel.sh
+++ gdal/gdal_parallel.sh
@@ -7,4 +7,4 @@ in=$1
 out=$(basename $in)
 
 #Change the coordinate system to EPSG:2393, which is the old Finnish YKJ (=KKJ3)
-gdalwarp $in $out -of COG -t_srs EPSG:2393
+gdalwarp $in $out -of COG -t_srs EPSG:2393 -overwrite
