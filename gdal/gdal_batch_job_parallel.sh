a05449695f6d528a7d96655539b06088e8a8dd22
Kylli Ek <kylli.ek@csc.fi>
2023-10-04 10:11:25 +0300

    Mark places to change

--- gdal/gdal_batch_job_parallel.sh
+++ gdal/gdal_batch_job_parallel.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# ToDo: change project name in the row below
 #SBATCH --account=project_200xxxx    # Choose the project to be billed
 #SBATCH --output=slurm-%j.out  # File to write the standard output to. %j is replaced by the job ID.
 #SBATCH --error=slurm-%j.err  # File to write the standard error to. %j is replaced by the job ID. Defaults to slurm-%j.out if not provided. 
 #SBATCH --time 0:05:00
 #SBATCH --partition=test  # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
 #SBATCH --nodes=1  # Number of compute nodes. Upper limit depends on partition.
-#SBATCH --cpus-per-task=4  # How many processors work on one task. Upper limit depends on number of CPUs per node.
+#SBATCH --cpus-per-task=3  # How many processors work on one task. Upper limit depends on number of CPUs per node.
 #SBATCH --mem-per-cpu=300  # Minimum memory required per usable allocated CPU.  Default units are megabytes.
 
 # Load geoconda module to have GDAL commandline tools available.
--- gdal/gdal_serial.sh
+++ gdal/gdal_serial.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Change to the folder where you have the input files
-cd /appl/data/geo/mml/dem10m/2019/V5/V53
-
 #Find the files that have .tif ending, we do not want to process the .tif.aux.xml files in the same folders.
-for i in $(find . -name '*.tif')
+for i in $(find /appl/data/geo/mml/dem10m/2019/V5/V53 -name '*.tif')
 
 #Process the files
 do
-    #Remove './' from the beginning of the find results. 
-    i=`echo $i | sed "s/\.\///"`
+    # Define output file name, based on input file name
+	out=$(basename $i)
 	#Change the coordinate system to EPSG:2393, which is the old Finnish YKJ (=KKJ3)
-    gdalwarp $i /scratch/project_200xxxx/geocomputing/gdal/$i -co compress=deflate -co tiled=yes -t_srs EPSG:2393
+	# ToDo: change project name and username in the row below
+    gdalwarp $i /scratch/project_200xxxx/students/cscusername/geocomputing/gdal/$i -co compress=deflate -co tiled=yes -t_srs EPSG:2393
 	# Add overviews
-    gdaladdo --config COMPRESS_OVERVIEW JPEG --config PHOTOMETRIC_OVERVIEW YCBCR --config INTERLEAVE_OVERVIEW PIXEL /scratch/project_200xxxx/geocomputing/gdal/$i 4 16 64
+	# ToDo: change project name and username in the row below
+    gdaladdo --config COMPRESS_OVERVIEW JPEG --config PHOTOMETRIC_OVERVIEW YCBCR --config \
+	INTERLEAVE_OVERVIEW PIXEL /scratch/project_200xxxx/students/cscusername/geocomputing/gdal/$i 4 16 64
 done
