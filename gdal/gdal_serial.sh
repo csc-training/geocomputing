9acc1502cf1356e4a89d20de8bdbeec7ad642bda
Kylli Ek <kylli.ek@csc.fi>
2023-10-04 08:48:17 +0300

    change project name

--- gdal/gdal_serial.sh
+++ gdal/gdal_serial.sh
@@ -11,7 +11,7 @@ do
     #Remove './' from the beginning of the find results. 
     i=`echo $i | sed "s/\.\///"`
 	#Change the coordinate system to EPSG:2393, which is the old Finnish YKJ (=KKJ3)
-    gdalwarp $i /scratch/project_2002044/geocomputing/gdal/$i -co compress=deflate -co tiled=yes -t_srs EPSG:2393
+    gdalwarp $i /scratch/project_200xxxx/geocomputing/gdal/$i -co compress=deflate -co tiled=yes -t_srs EPSG:2393
 	# Add overviews
-    gdaladdo --config COMPRESS_OVERVIEW JPEG --config PHOTOMETRIC_OVERVIEW YCBCR --config INTERLEAVE_OVERVIEW PIXEL /scratch/project_2002044/geocomputing/gdal/$i 4 16 64
+    gdaladdo --config COMPRESS_OVERVIEW JPEG --config PHOTOMETRIC_OVERVIEW YCBCR --config INTERLEAVE_OVERVIEW PIXEL /scratch/project_200xxxx/geocomputing/gdal/$i 4 16 64
 done
