df933ed3ca9bf8d751671a9684fe1a0fb599417f
Kylli Ek <kylli.ek@csc.fi>
2023-10-04 10:16:48 +0300

    minor fixes

--- gdal/gdal_batch_job_parallel.sh
+++ gdal/gdal_batch_job_parallel.sh
@@ -15,5 +15,5 @@ module load parallel geoconda
 # Find the files that have .tif ending, we do not want to process the .tif.aux.xml files in the same folders.
 # Run the GDAL script for each found file.
 
-find find /appl/data/geo/mml/dem10m/2019/W3/W33 -name '*.tif' | \
+find /appl/data/geo/mml/dem10m/2019/W3/W33 -name '*.tif' | \
     parallel -j $SLURM_CPUS_PER_TASK bash gdal_parallel.sh {}
