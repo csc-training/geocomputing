bbb44a7f269330b8abd3a84be3c662d449612c2f
samumantha <samantha.wittke@csc.fi>
2023-10-07 22:17:42 +0300

    line up comments

--- python/puhti/01_serial/single_core_example_folder.sh
+++ python/puhti/01_serial/single_core_example_folder.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --account=project_200xxxx    # Choose the project to be billed
-#SBATCH --output=slurm-%j.out  # File to write the standard output to. %j is replaced by the job ID.
-#SBATCH --error=slurm-%j.err  # File to write the standard error to. %j is replaced by the job ID. Defaults to slurm-%j.out if not provided. 
-#SBATCH --time=00:05:00  # Maximum duration of the job. Upper limit depends on partition.
-#SBATCH --ntasks=1  # Number of tasks. Upper limit depends on partition.
-#SBATCH --cpus-per-task=1  # How many processors work on one task. Upper limit depends on number of CPUs per node.
-#SBATCH --mem-per-cpu=4G  # Minimum memory required per usable allocated CPU.  Default units are megabytes.
-#SBATCH --partition=test  # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
+#SBATCH --account=project_200xxxx   # Choose the project to be billed
+#SBATCH --output=slurm-%j.out       # File to write the standard output to. %j is replaced by the job ID.
+#SBATCH --error=slurm-%j.err        # File to write the standard error to. %j is replaced by the job ID. Defaults to slurm-%j.out if not provided. 
+#SBATCH --time=00:05:00             # Maximum duration of the job. Upper limit depends on partition.
+#SBATCH --ntasks=1                  # Number of tasks. Upper limit depends on partition.
+#SBATCH --cpus-per-task=1           # How many processors work on one task. Upper limit depends on number of CPUs per node.
+#SBATCH --mem-per-cpu=4G            # Minimum memory required per usable allocated CPU.  Default units are megabytes.
+#SBATCH --partition=test            # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
 
 module load geoconda
 
--- python/puhti/02_gnu_parallel/gnu_parallel_example.sh
+++ python/puhti/02_gnu_parallel/gnu_parallel_example.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --account=project_200xxxx    # Choose the project to be billed
-#SBATCH --time=00:05:00  # Maximum duration of the job. Upper limit depends on partition.
-#SBATCH --ntasks=1  # Number of tasks. Upper limit depends on partition.
-#SBATCH --cpus-per-task=3  # How many processors work on one task. Upper limit depends on number of CPUs per node.
-#SBATCH --mem-per-cpu=4G  # Minimum memory required per usable allocated CPU.  Default units are megabytes.
-#SBATCH --partition=test  # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
+#SBATCH --account=project_200xxxx   # Choose the project to be billed
+#SBATCH --time=00:05:00             # Maximum duration of the job. Upper limit depends on partition.
+#SBATCH --ntasks=1                  # Number of tasks. Upper limit depends on partition.
+#SBATCH --cpus-per-task=3           # How many processors work on one task. Upper limit depends on number of CPUs per node.
+#SBATCH --mem-per-cpu=4G            # Minimum memory required per usable allocated CPU.  Default units are megabytes.
+#SBATCH --partition=test            # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
 
 module load parallel
 module load geoconda
