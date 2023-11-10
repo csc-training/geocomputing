7c4f8852fd59ae7b1b3cfc74b933e08e0681d3e9
Kylli Ek <kylli.ek@csc.fi>
2023-11-10 09:42:46 +0200

    Add example batch job files

---
+++ python/STAC/csc_stac_example.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This example shows how to use STAC, dask and xarray via Python script. 
+# If new to STAC, see the STAC_CSC_example.ipynb for longer explanation how STAC works.
+# In this example, we will search and download data through an STAC Catalog and process it using Dask and Xarray. 
+# We will use Sentinel-1 data stored at FMI to compute a mean value of vv_mean for one month. The result will be saved to a new GeoTiff file.
+
+import requests
+import stackstac
+from dask.distributed import Client, Lock
+import pystac_client
+import rioxarray
+import sys
+import os
+
+#Settings
+STAC_URL = "https://paituli.csc.fi/geoserver/ogc/stac/v1"
+collection = 'sentinel_1_11_days_mosaics_at_fmi'
+time_filter="2021-03-01/2021-03-31"
+asset = 'mean_vv'
+output_file = os.path.join(os.getcwd(), "sentinel1_mean_vv.tif")
+
+# Use as many workers than you have available cores
+no_of_workers = len(os.sched_getaffinity(0))
+
+
+# To overcome a current bug, rewrite requests to https. Needed only with Paituli STAC.
+def change_to_https(request: requests.Request) -> requests.Request: 
+    request.url = request.url.replace("http:", "https:")
+    return request
+
+def find_items_from_stac():
+    catalog = pystac_client.Client.open(STAC_URL, request_modifier=change_to_https)
+    search_bbox = catalog.search(
+        collections=[collection],
+        datetime=time_filter
+    )
+    return search_bbox.item_collection()
+
+def main():
+
+    # Create Dask client, because the process is slowed down by data download speed, then it usually is good to use 1 core workers.
+    client = Client(n_workers=no_of_workers)
+
+    item_collection = find_items_from_stac()
+
+    # Use the `stackstac` library to convert item collection to Xarray DataArray. 
+    cube = stackstac.stack(
+        items=item_collection,
+        assets=[asset],
+        #chunksize=(-1,1,2046,2046),
+        epsg=3067
+    ).squeeze() 
+    
+    # Create new data cube for the mean value. 
+    mean = cube.mean("time", keep_attrs=True)
+
+    # Compute and save the result
+    mean_ndvi_tiff = mean.rio.to_raster(
+        output_file,
+        lock=Lock(name="rio", client=client),
+        tiled=True,
+    )
+   
+    #Close Dask cluster
+    client.close()
+
+# With Dask, it is important that to use main function
+if __name__ == "__main__":
+    main()
+    print("Analysis ready")
+
---
+++ python/STAC/csc_stac_example_batch_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --account=project_2000599    # Choose the project to be billed
+#SBATCH --time=00:20:00  # Maximum duration of the job. Upper limit depends on partition.
+#SBATCH --ntasks=1  # Number of tasks. Upper limit depends on partition.
+#SBATCH --cpus-per-task=10  # How many processors work on one task. Upper limit depends on number of CPUs per node.
+#SBATCH --mem=100G  # Minimum memory required per usable allocated CPU.  Default units are megabytes.
+#SBATCH --partition=small  # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
+
+# Load the geoconda module which has Python with Dask, Xarray and STAC libraries
+module load geoconda
+
+# Run the Python code. 
+python csc_stac_example.py
