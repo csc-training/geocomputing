e1eac30fcef7cef9eedceebdeff301e2b5cc9209
Samantha Wittke <32324155+samumantha@users.noreply.github.com>
2024-02-02 10:45:03 +0200

    comments added

--- Copernicus_data_download/CDSE_to_Allas_object_storage.sh
+++ Copernicus_data_download/CDSE_to_Allas_object_storage.sh
@@ -1,40 +1,52 @@
 #!/bin/bash
 
 ###############
-# Script to 
+# Example script to 
 # 1. Query Copernicus Data Space Ecosystem Sentinel-2 catalog based on startdate, enddate, cloudcover and tilename 
 # 2. Download the found data from CDSE object storage to Allas object storage
 #
-# Requirements: Rclone setup to work with allas as [s3allas] (s3 setup connected to CSC project for Allas) and CDSE as [cdse] 
+# Requirements: Rclone setup to work with allas as [s3allas] (s3 setup connected to CSC project for Allas) and CDSE as [cdse].
+# See [CSC EO guide](https://docs.csc.fi/support/tutorials/gis/eo_guide/) about how to set it up. 
 #
-# Written by Samantha Wittke, CSC - IT center for Science, based on script provided by Maria Yli-Heikkilä
+# Based on script provided by Maria Yli-Heikkilä (LUKE) - adapted to CDSE by Samantha Wittke, CSC - IT center for Science
 
-TILES=("T34VDM" "T34VEM" "T34VEN" "T34VEP" "T34VEQ" "T34VER" "T34VFM" "T34VFN" "T34VFP" "T34VFQ" "T34VFR" "T34WFS" "T34WFT" "T34WFU" "T35VLG" "T35VLH" "T35VLJ" "T35VLK" "T35VLL" "T35VMG" "T35VMH" "T35VMJ" "T35VMK" "T35VML" "T35VNH" "T35VNJ" "T35VNK" "T35VNL" "T35VPJ" "T35VPK" "T35VPL" "T35WMM" "T35WMN" "T35WNM" "T36VUP" "T36VUQ" "T36VUR") 
-BASEURL="http://catalogue.dataspace.copernicus.eu/resto/api/collections/Sentinel2/search.json?"
-BUCKETBASENAME="Sentinel2-MSIL2A-cloud-0-95"
+# Provide Sentinel-2 tilenames that you want to download
+TILES=("T34VDM" "T34VEM" "T34VEN")
 
-LASTDATE=2023-05-01T00:00:00
-STARTDATE=$LASTDATE
+# Provide your bucketname for Allas; bucketname is then constructed: BUCKETBASENAME-YEAR-TILE
+BUCKETBASENAME="your_bucket_name"
 
+# Provide the timedelta - start date and time
+STARTDATE=2023-05-01T00:00:00
+
+# end date and time ; fixed or set to current date
 #CURRENTDATE=$(date +"%Y-%m-%dT%T")
 #ENDDATE=$CURRENTDATE
 ENDDATE=2023-05-06T23:59:59
+
+CLOUDCOVER="[0,95]"
+
+# Baseurl to reach the CDSE catalog with json output
+BASEURL="http://catalogue.dataspace.copernicus.eu/resto/api/collections/Sentinel2/search.json?"
 
 YEAR=${ENDDATE:0:4}
 
 for TILE in ${TILES[@]}
 
 do
-    QUERY="productType=S2MSI2A&startDate=$STARTDATE.000Z&completionDate=${ENDDATE}.000Z&cloudCover=[0,95]&productIdentifier=$TILE"
-    echo $BASEURL$QUERY
-    wget --output-document=query_$DATE_$TILE.json $BASEURL$QUERY
+    # Query the catalog with previously defined variables
+    QUERY="productType=S2MSI2A&startDate=${STARTDATE}.000Z&completionDate=${ENDDATE}.000Z&cloudCover=${CLOUDCOVER}&productIdentifier=${TILE}"
+    # echo $BASEURL$QUERY
+    wget --output-document=query_$YEAR_$TILE.json $BASEURL$QUERY
 
-    #get only real product paths from json output
-    jq -r  '.. | .productIdentifier? | select( . != null ) ' query_$TILE.json  | grep "/eodata/" > name_$TILE.txt
+    # JSON includes much more information than only product paths -> extract product path from the JSON
+    jq -r  '.. | .productIdentifier? | select( . != null ) ' query_$YEAR_$TILE.json  | grep "/eodata/" > name_$TILE.txt
 
+    # Read the file with product paths and download each file from CDSE to Allas bucket defined above
     while IFS="" read -r FILE || [ -n "$FILE" ]
     do
         echo $FILE
+        # Rclone needs the filename in destination, otherwise the source .SAFE directory is unpacked without the .SAFE directory
         SAFENAME="$(basename -- $FILE)"
         rclone copy -P -v cdse:$FILE s3allas:$BUCKETBASENAME-$YEAR-$TILE/$SAFENAME
     done < name_$DATE_$TILE.txt
