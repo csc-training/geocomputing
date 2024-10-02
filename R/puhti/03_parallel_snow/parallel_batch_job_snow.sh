3b19a7f7ea71db61462535c762258a24fc906416
EetuHuuskoCSC <116141296+EetuHuuskoCSC@users.noreply.github.com>
2024-10-02 14:27:15 +0300

    Update parallel_batch_job_snow.sh

--- R/puhti/03_parallel_snow/parallel_batch_job_snow.sh
+++ R/puhti/03_parallel_snow/parallel_batch_job_snow.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --account=project_200xxxx    # Choose the project to be billed
+#SBATCH --account=project_20xxxxx    # Choose the project to be billed
 #SBATCH --output=output_%j.txt  # File to write the standard output to.
 #SBATCH --error=errors_%j.txt  # File to write the standard error to.
 #SBATCH --time=00:04:00  # Maximum duration of the job. Upper limit depends on partition.
