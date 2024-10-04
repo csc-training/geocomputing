79ad59a74c800b405493044150a5452f70184b90
EetuHuuskoCSC <116141296+EetuHuuskoCSC@users.noreply.github.com>
2024-10-04 16:15:23 +0300

    Update dask_multinode.sh

--- python/puhti/06_parallel_dask/multi_node/dask_multinode.sh
+++ python/puhti/06_parallel_dask/multi_node/dask_multinode.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1                  # Number of tasks. Upper limit depends on partition.
 #SBATCH --cpus-per-task=1           # How many processors work on one task. Upper limit depends on number of CPUs per node.
 #SBATCH --mem-per-cpu=6G            # Memory required per allocated CPU.  Default units are megabytes.
-#SBATCH --partition=test            # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
+#SBATCH --partition=small            # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
 
 # The resources reserved here are only for the master job, so 1 core and moderate memory should be enough.
 # The resources for workers are reservd in the Python file.
