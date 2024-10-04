fc3af5f873fa98fe623ca562011c9efc11ef969c
EetuHuuskoCSC <116141296+EetuHuuskoCSC@users.noreply.github.com>
2024-10-04 15:34:15 +0300

    Update single_core_example.sh

--- python/puhti/01_serial/single_core_example.sh
+++ python/puhti/01_serial/single_core_example.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1                  # Number of tasks. Upper limit depends on partition.
 #SBATCH --cpus-per-task=1           # How many processors work on one task. Upper limit depends on number of CPUs per node.
 #SBATCH --mem-per-cpu=2G            # Minimum memory required per usable allocated CPU.  Default units are megabytes.
-#SBATCH --partition=test            # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
+#SBATCH --partition=small            # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
 
 module load geoconda
 
