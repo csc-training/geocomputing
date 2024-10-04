0e5a9c1d618f0ab3f57394c3769708dfc7d34c75
EetuHuuskoCSC <116141296+EetuHuuskoCSC@users.noreply.github.com>
2024-10-04 16:17:19 +0300

    Update parallel_batch_job_future_multicore.sh

--- R/puhti/02_parallel_future/parallel_batch_job_future_multicore.sh
+++ R/puhti/02_parallel_future/parallel_batch_job_future_multicore.sh
@@ -5,7 +5,7 @@
 #SBATCH --error=errors.txt  # File to write the standard error to.
 #SBATCH --time=00:05:00  # Maximum duration of the job. Upper limit depends on partition.
 #SBATCH --ntasks=1  # Number of tasks. Upper limit depends on partition.
-#SBATCH --partition=test  # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
+#SBATCH --partition=small  # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
 #SBATCH --mem-per-cpu=1000  # Minimum memory required per usable allocated CPU.  Default units are megabytes.
 #Equal to number of workers. Max 40 in Puhti.
 #SBATCH --cpus-per-task=3  # How many processors work on one task. Upper limit depends on number of CPUs per node.
