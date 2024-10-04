74b04e1cd4854fcf332f556cd8e30ea142aaef8d
EetuHuuskoCSC <116141296+EetuHuuskoCSC@users.noreply.github.com>
2024-10-04 16:16:54 +0300

    Update serial_batch_job.sh

--- R/puhti/01_serial/serial_batch_job.sh
+++ R/puhti/01_serial/serial_batch_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=slurm-%j.out  # File to write the standard output to. %j is replaced by the job ID.
 #SBATCH --error=slurm-%j.err  # File to write the standard error to. %j is replaced by the job ID. Defaults to slurm-%j.out if not provided. 
 #SBATCH --time=0:05:00  # Maximum duration of the job. Upper limit depends on partition.
-#SBATCH --partition=test  # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
+#SBATCH --partition=small  # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
 #SBATCH --nodes=1  # Number of compute nodes. Upper limit depends on partition.
 #SBATCH --ntasks=1  # Number of tasks. Upper limit depends on partition.
 #SBATCH --mem-per-cpu=1000  # Minimum memory required per usable allocated CPU.  Default units are megabytes.
