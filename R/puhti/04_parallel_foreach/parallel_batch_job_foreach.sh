18d526af8e9f582f04f9e16c2ae941b0f50e7861
EetuHuuskoCSC <116141296+EetuHuuskoCSC@users.noreply.github.com>
2024-10-04 16:17:46 +0300

    Update parallel_batch_job_foreach.sh

--- R/puhti/04_parallel_foreach/parallel_batch_job_foreach.sh
+++ R/puhti/04_parallel_foreach/parallel_batch_job_foreach.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=00:05:00  # Maximum duration of the job. Upper limit depends on partition.
 #SBATCH --ntasks=3  # Number of tasks. Upper limit depends on partition.
 #Test partition is used for testing, for real jobs use either serial or parallel depending on how many nodes you need.
-#SBATCH --partition=test  # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
+#SBATCH --partition=small  # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
 #SBATCH --mem-per-cpu=1000  # Minimum memory required per usable allocated CPU.  Default units are megabytes.
 
 module load r-env
