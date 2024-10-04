8320992a29b525a70de215651b8e14a456b94e05
EetuHuuskoCSC <116141296+EetuHuuskoCSC@users.noreply.github.com>
2024-10-04 16:17:08 +0300

    Update parallel_batch_job_future_cluster.sh

--- R/puhti/02_parallel_future/parallel_batch_job_future_cluster.sh
+++ R/puhti/02_parallel_future/parallel_batch_job_future_cluster.sh
@@ -7,7 +7,7 @@
 #Reserve cores for 1 master + 3 workers
 #SBATCH --ntasks=4  # Number of tasks. Upper limit depends on partition.
 #Test partition is used for testing, for real jobs use either serial or parallel depending on how many nodes you need.
-#SBATCH --partition=test  # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
+#SBATCH --partition=small  # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
 #SBATCH --mem-per-cpu=1000  # Minimum memory required per usable allocated CPU.  Default units are megabytes.
 
 module load r-env
