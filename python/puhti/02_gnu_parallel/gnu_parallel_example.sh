c6e709393d67699448e72cb951ca8bb3b39fa19c
EetuHuuskoCSC <116141296+EetuHuuskoCSC@users.noreply.github.com>
2024-10-04 16:12:28 +0300

    Update gnu_parallel_example.sh

--- python/puhti/02_gnu_parallel/gnu_parallel_example.sh
+++ python/puhti/02_gnu_parallel/gnu_parallel_example.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1                  # Number of tasks. Upper limit depends on partition.
 #SBATCH --cpus-per-task=3           # How many processors work on one task. Upper limit depends on number of CPUs per node.
 #SBATCH --mem-per-cpu=2G            # Minimum memory required per usable allocated CPU.  Default units are megabytes.
-#SBATCH --partition=test            # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
+#SBATCH --partition=small            # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
 
 module load parallel
 module load geoconda
