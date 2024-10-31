2c88bf950ff605fb92a2e063c975bbe7007b2556
EetuHuuskoCSC <116141296+EetuHuuskoCSC@users.noreply.github.com>
2024-10-31 09:10:53 +0200

    Update array_job_example.sh

--- python/puhti/03_array/array_job_example.sh
+++ python/puhti/03_array/array_job_example.sh
@@ -2,7 +2,7 @@
 #SBATCH --output=slurm-%A_%a.out    # File to write the standard output to. %A is replaced by the job ID and %a with the array index.
 #SBATCH --error=slurm-%A_%a.err     # File to write the standard error to. %A is replaced by the job ID and %a with the array index. Defaults to slurm-%A_%a.out if not provided.
 #SBATCH --account=project_20xxxxx   # Choose the project to be billed
-#SBATCH --reservation=geocomputing_thu # Only available during the course
+# SBATCH --reservation=geocomputing_thu # Only available during the course
 #SBATCH --time=00:05:00             # Maximum duration of the job. Upper limit depends on partition.
 #SBATCH --ntasks=1                  # Number of tasks. Upper limit depends on partition.
 #SBATCH --cpus-per-task=1           # How many processors work on one task. Upper limit depends on number of CPUs per node.
