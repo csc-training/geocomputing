dd85cd3b166f8fd57574dc19176f43cf7d0bcb23
Kylli Ek <kylli.ek@csc.fi>
2026-07-22 10:18:02 +0300

    Enhance process_one_file.sh with comments and userdir setup
    Added comments for clarity and improved user directory handling.

--- snap/process_one_file.sh
+++ snap/process_one_file.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
+# Read the input and output files from argument
 LINE="$1"
-
-echo "$LINE"
-
 # Everything before ; is input files
 FILES="${LINE%;*}"
 # After ; is the output file name
 OUTPUT="${LINE#*;}"
 
-echo "$FILES"
-echo "$OUTPUT"
-
+# Set userdir, separate for each set of inputs.
+# Each job in Roihu as by default 20 Gb space in $TMPDIR, if that is enough, use it. 
+# Note that with `xargs` parallel case this 20 Gb is common for all jobs.
+# If that is not enough, use scratch.
 JOBID=$$
-
 USERDIR="$TMPDIR/snap_user_${JOBID}"
 #USERDIR="/scratch/project_2000599/tmp/snap_user_${JOBID}"
-
+# Create the folder
 mkdir -p "$USERDIR"
 
+# Run the SNAP GPT graph
+# -q 2 - how many cores to use for calculating one GPT graph. This depends on task and data, so test different values. Also 1 could be a good choice.
+# -c 5G - tile cache size, depends on task..
+# -J-Xmx55G - Java maximum heap memory
+# -c and -J-Xmx55G together shoudl be less than memory reserved in the batch job.
 gpt snap_graph_stacking.xml \
     -J-Dsnap.userdir="$USERDIR" \
     -PfileList="$FILES" \
     -PoutputFile="$OUTPUT" \
-    -q  $SLURM_CPUS_PER_TASK -c 5G -J-Xmx14G -e
+    -q  $SLURM_CPUS_PER_TASK \
+    -c 5G \
+    -J-Xmx14G -e
 
-# Match values in gpt command with job reservation: 
-# -q 2 with --cpus-per-task=2
-# -J-Xmx55G with --mem=60000, use for job a few Gb less than reserved
-# -c 40G with -J-Xmx55G, use ~75 % of available memory for data cache, depends on task..
-
+# If using scratch, delete the userdir as last step.
 #rm -r "$USERDIR"
